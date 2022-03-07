5a413930b63202ba61eb1f9581a80341edbb93e2
Levi Hackerman <62335141+thewickedkarma@users.noreply.github.com>
2022-03-07 09:51:59 +0530

    Some rofi and icons

---
+++ .config/tint2/rofi/shutdown.sh
@@ -0,0 +1,60 @@
+0#!/usr/bin/env bash
+uptime=$(uptime -p | sed -e 's/up //g')
+theme="~/.config/tint2/rofi/card_sqaure.rasi"
+rofi_command="rofi -theme $theme"
+
+# Options
+shutdown=""
+reboot=""
+lock=""
+suspend=""
+logout=""
+
+# Confirmation
+confirm_exit() {
+	rofi -dmenu\
+		-i\
+		-no-fixed-num-lines\
+		-p "Are You Sure? : "\
+		-theme $dir/confirm.rasi
+}
+
+# Message
+msg() {
+	rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
+}
+
+# Variable passed to rofi
+options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 2)"
+case $chosen in
+    $shutdown)
+		shutdown now
+        ;;
+    $reboot)
+	    reboot
+        ;;
+    $lock)
+		xflock4
+        ;;
+    $suspend)
+		systemctl suspend
+        ;;
+    $logout)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+				openbox --exit
+			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+				bspc quit
+			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+				i3-msg exit
+			fi
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+esac
---
+++ .config/tint2/rofi/time.sh
@@ -0,0 +1,15 @@
+dir="~/.config/tint2/rofi"
+rofi_command="rofi -theme $dir/time.rasi"
+
+## Get time and date
+TIME="$(date +"%I:%M %p")"
+DN=$(date +"%A")
+MN=$(date +"%B")
+DAY="$(date +"%d")"
+MONTH="$(date +"%m")"
+YEAR="$(date +"%Y")"
+
+options="$DAY\n$MONTH\n$YEAR"
+
+## Main
+chosen="$(echo -e "$options" | $rofi_command -p "   $DN, $TIME" -dmenu -selected-row 1)"
